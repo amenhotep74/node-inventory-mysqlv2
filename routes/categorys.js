dffce2caab1970bed96f4aded8886a8affa77181
amenhotep74 <Blewie144@gmail.com>
2020-04-09 08:25:33 +0930

    added view route for categorys

--- routes/categorys.js
+++ routes/categorys.js
@@ -1,4 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'node-inventory-mysql',
+  debug: false,
+});
+
+// List all items localhost/categorys
+
+router.get('/', function (req, res, next) {
+  db.query('SELECT * FROM categorys', function (err, rs) {
+    res.render('categorys', { categorys: rs });
+  });
+});
 
 module.exports = router;
